d22d562e33904d3e1807e94966a701be932a5a75
srimanikantaBattu <srimanikantabattu04@gmail.com>
2025-06-18 15:53:52 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_URL);
   return (
     <div className="">
       <p>Hello Checkings</p>
+      <p></p>
     </div>
   )
 }
